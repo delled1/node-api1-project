54648d3cf49ca68059e18d9e2b5123438a1675af
Eric Della <ericdella13@gmail.com>
2021-02-10 14:11:55 -0800

    GET returns array of users, POST creates new user, both working

--- api/server.js
+++ api/server.js
@@ -1,3 +1,45 @@
 // BUILD YOUR SERVER HERE
+const express = require("express")
+const db = require("./users/model")
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json())
+
+
+server.get("/users/" , (req, res) => {
+    db.find()
+    .then(users => {
+    // console.log("Fetching:" res);
+    res.status(200).json(users);
+    })
+    .catch(error => {
+        res.status(500).json({ message: error });
+    });
+})
+
+// server.get("/users/:id", (req, res) => {
+//     const user = db.findById(req.params.id)
+
+//     if (user) {
+//         res.json(user)
+//     } else{
+//         res.status(404).json({
+//             message: "USER NOT FOUND"
+//         })
+//     }
+// })
+
+server.post("/users", (req, res) => {
+    const newUser = db.insert({
+        name: req.body.name,
+        bio: req.body.bio
+    })
+
+    res.status(201).json(newUser)
+})
+
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
